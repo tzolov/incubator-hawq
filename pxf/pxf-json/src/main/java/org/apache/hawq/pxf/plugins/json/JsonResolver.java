186dd121c66c23576b208b602625650724c1084d
tzolov <christian.tzolov@gmail.com>
2016-02-02 00:08:12 +0100

    HAWQ-178: Address all comments. Remove redundant code

--- pxf/pxf-json/src/main/java/org/apache/hawq/pxf/plugins/json/JsonResolver.java
+++ pxf/pxf-json/src/main/java/org/apache/hawq/pxf/plugins/json/JsonResolver.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
